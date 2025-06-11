2ad3bf4190c1b1a57045023b923d83e05c8749c2
anntreasajojo <anntreasa.jojo@gmail.com>
2025-06-11 15:42:53 -0700

    Created several functions: 1. Gather information from UI (aka which buttons the user selected) 2. Highlight which button the user selected 3. Calculate based on the selections for custom rest time.
    Added 3 TODO's aswell.

--- app/src/main/java/com/example/restright/MainActivity.java
+++ app/src/main/java/com/example/restright/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.restright;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,6 +13,10 @@ import com.example.restright.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
+    String clickedSkillButton = "";
+    String clickedExerciseGoalButton = "";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +25,50 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        getInformationFromDisplay();
+    }
+
+    private void calculateRest(String skillLevel, String exerciseGoal) {
+        //key notes
+        //beginner + strength = 300 seconds
+        //beginner + muscle mass = 90 seconds
+        //beginner + endurance = 60 seconds
+
+        //intermediate + strength = 180 seconds
+        //intermediate + muscle mass = 60 seconds
+        //intermediate + endurance = 40 seconds
+
+        //advanced + strength = 120 seconds
+        //advanced + muscle mass = 30 seconds
+        //advanced + endurance = 20 seconds
+
+        if(skillLevel.equalsIgnoreCase("Beginner") && exerciseGoal.equalsIgnoreCase("Strength")){
+            binding.calculatedTimeTextView.setText("300 seconds");
+        }else if(skillLevel.equalsIgnoreCase("Beginner") && exerciseGoal.equalsIgnoreCase("Muscle Mass")){
+            binding.calculatedTimeTextView.setText("90 seconds");
+        }else if(skillLevel.equalsIgnoreCase("Beginner") && exerciseGoal.equalsIgnoreCase("Endurance")){
+            binding.calculatedTimeTextView.setText("60 seconds");
+        }
+
+        if(skillLevel.equalsIgnoreCase("Intermediate") && exerciseGoal.equalsIgnoreCase("Strength")){
+            binding.calculatedTimeTextView.setText("180 seconds");
+        }else if(skillLevel.equalsIgnoreCase("Intermediate") && exerciseGoal.equalsIgnoreCase("Muscle Mass")){
+            binding.calculatedTimeTextView.setText("60 seconds");
+        }else if(skillLevel.equalsIgnoreCase("Intermediate") && exerciseGoal.equalsIgnoreCase("Endurance")){
+            binding.calculatedTimeTextView.setText("40 seconds");
+        }
+
+        if(skillLevel.equalsIgnoreCase("Advanced") && exerciseGoal.equalsIgnoreCase("Strength")){
+            binding.calculatedTimeTextView.setText("120 seconds");
+        }else if(skillLevel.equalsIgnoreCase("Advanced") && exerciseGoal.equalsIgnoreCase("Muscle Mass")){
+            binding.calculatedTimeTextView.setText("30 seconds");
+        }else if(skillLevel.equalsIgnoreCase("Advanced") && exerciseGoal.equalsIgnoreCase("Endurance")){
+            binding.calculatedTimeTextView.setText("20 seconds");
+        }
+
+    }
+
+    private void getInformationFromDisplay() {
         //must select a skill type
         //must select a exercise goal
         //total of 9 different button combination
@@ -37,20 +86,114 @@ public class MainActivity extends AppCompatActivity {
         //from healthline.com "Muscular endurance training typically involves short rest periods between 20–60 seconds."
         //endurance = 20-60 seconds
 
+        ///TODO: make it so that the user is only able to click one option for skill level
+        //get skill level from UI
 
-        //key notes
-        //beginner + strength = 300 seconds
-        //beginner + muscle mass = 90 seconds
-        //beginner + endurance = 60 seconds
+        //user clicked beginner
+        binding.skillLevelBeginnerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickedSkillButton = "Beginner";
+                highlightSelectedOption("Beginner");
+            }
+        });
 
-        //intermediate + strength = 180 seconds
-        //intermediate + muscle mass = 60 seconds
-        //intermediate + endurance = 40 seconds
+        //user clicked intermediate
+        binding.skillLevelIntermediateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickedSkillButton = "Intermediate";
+                highlightSelectedOption("Intermediate");
+            }
+        });
 
-        //advanced + strength = 120 seconds
-        //advanced + muscle mass = 30 seconds
-        //advanced + endurance = 30 seconds
+        //user clicked advanced
+        binding.skillLevelAdvancedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickedSkillButton = "Advanced";
+                highlightSelectedOption("Advanced");
+            }
+        });
+
+        ///TODO: make it so that the user is only able to click one option for exercise goal
+        //get exercise goal from UI
+
+        //user clicked strength
+        binding.strengthGoalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickedExerciseGoalButton = "Strength";
+                highlightSelectedOption("Strength");
+            }
+        });
+
+        //user clicked muscle mass
+        binding.muscleMassGoalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickedExerciseGoalButton = "Muscle Mass";
+                highlightSelectedOption("Muscle Mass");
+
+            }
+        });
+
+        //user clicked endurance
+        binding.enduranceGoalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickedExerciseGoalButton = "Endurance";
+                highlightSelectedOption("Endurance");
+            }
+        });
+
+        binding.calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateRest(clickedSkillButton, clickedExerciseGoalButton);
+            }
+        });
+
+
+
 
 
     }
+
+    private void highlightSelectedOption(String option){
+        ///TODO: create a reset button that lets you calculate other rest time combinations
+
+        if(option.equalsIgnoreCase("Beginner")){
+            binding.skillLevelBeginnerButton.setBackgroundColor(Color.parseColor("#FFFFFF"));
+            binding.skillLevelBeginnerButton.setTextColor(Color.parseColor("#948979"));
+        }
+
+        if(option.equalsIgnoreCase("Intermediate")){
+            binding.skillLevelIntermediateButton.setBackgroundColor(Color.parseColor("#FFFFFF"));
+            binding.skillLevelIntermediateButton.setTextColor(Color.parseColor("#948979"));
+        }
+
+        if(option.equalsIgnoreCase("Advanced")){
+            binding.skillLevelAdvancedButton.setBackgroundColor(Color.parseColor("#FFFFFF"));
+            binding.skillLevelAdvancedButton.setTextColor(Color.parseColor("#948979"));
+        }
+
+        if(option.equalsIgnoreCase("Strength")){
+            binding.strengthGoalButton.setBackgroundColor(Color.parseColor("#FFFFFF"));
+            binding.strengthGoalButton.setTextColor(Color.parseColor("#948979"));
+        }
+
+        if(option.equalsIgnoreCase("Muscle Mass")){
+            binding.muscleMassGoalButton.setBackgroundColor(Color.parseColor("#FFFFFF"));
+            binding.muscleMassGoalButton.setTextColor(Color.parseColor("#948979"));
+        }
+
+        if(option.equalsIgnoreCase("Endurance")){
+            binding.enduranceGoalButton.setBackgroundColor(Color.parseColor("#FFFFFF"));
+            binding.enduranceGoalButton.setTextColor(Color.parseColor("#948979"));
+        }
+
+    }
+
+
 }
